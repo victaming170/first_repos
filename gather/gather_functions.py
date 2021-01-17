7c1446928763fca1eca0fe6e6497cd355d5f0994
victaming170 <victaming17@gmail.com>
2021-01-17 19:45:10 +0800

    upd

--- gather/gather_functions.py
+++ gather/gather_functions.py
@@ -1,4 +1,5 @@
 
+# compare 2 files
 def file_cmp_text(file0_, file1_):
     try:
         f0 = open(file0_, 'r', encoding='utf-8')
@@ -9,7 +10,7 @@ def file_cmp_text(file0_, file1_):
         f1 = open(file1_, 'r', encoding='utf-8')
     except FileNotFoundError:
         f1 = open(file1_, 'w')
-        print('>Creat ', end='')
+        print('[Creat]', end='')
         f1 = open(file1_, 'r', encoding='utf-8')
     #  compare
     while True:
@@ -26,7 +27,7 @@ def file_cmp_text(file0_, file1_):
             f1.close()
             return False
 
-
+# copy text file from file0 to file1
 def copy_file_text(file_src_, file_dst_):
     f_src = open(file_src_, 'r', encoding='utf-8')
     f_dst = open(file_dst_, 'w', encoding='utf-8')
@@ -40,6 +41,7 @@ def copy_file_text(file_src_, file_dst_):
     f_dst.close()
 
 
+# strip filename from path
 def get_filename_from_path(path_):
     try:
         x_index = path_.rfind('/')
@@ -50,3 +52,35 @@ def get_filename_from_path(path_):
     else:
         return path_[x_index+1:]
 
+
+# show user options, get the choice of user
+def option_button(layer_, default_, *options_):
+    # show the option layer
+    print(f'Option_{layer_}:')
+    options_n = len(options_)
+    # get default choice
+    try:
+        default_ = int(default_)
+    except ValueError:
+        default_ = 0
+        print('x(Invalid default option, reset to 0.)x')
+    else:
+        if default_ >= options_n:
+            default_ = 0
+            print('x(Invalid default option, reset to 0.)x')
+    # show options
+    for n in range(options_n):
+        if n != default_:
+            print(f'({n}) {options_[n]}')
+        else:
+            print(f'[{n}] {options_[n]}')
+    while True:
+        button = input('>>>')
+        if not button:
+            button = default_
+            break
+        elif button in [str(n) for n in range(options_n)]:
+            break
+        else:
+            print('x(Invalid option, try again pls.)x')
+    return int(button)
--- gather/gather_man.py
+++ gather/gather_man.py
@@ -2,39 +2,70 @@
 Gather files to local Github_Repository.
 '''
 import json
+from pathlib import Path
 import gather_functions as fct
 
-with open('filenotes.json', 'r') as fj:
-    filenotes = json.load(fj)
-    # print(filenotes)
-    src_root_dir = filenotes["src_root_dir"]
-    dst_root_dir = filenotes["dst_root_dir"]
-    src_list = filenotes["src_list"]
-    dst_list = filenotes["dst_list"]
+print('==== This is GatherMan. ====')
 
-# print(src_root_dir, src_list)
-# print(dst_root_dir, dst_list)
+while True:
+    choose0 = fct.option_button(0, 1, 'Exit', 'Update', 'Browse existing file_couple',\
+        'New file_couple', 'Change root directory')
 
-notes_n = len(src_list)
-for n in range(notes_n):
-    src_name = fct.get_filename_from_path(src_list[n])
-    dst_name = fct.get_filename_from_path(dst_list[n])
-    if src_name == dst_name:
-        src_path = src_root_dir + src_list[n]
-        dst_path = dst_root_dir + dst_list[n]
-        if not fct.file_cmp_text(src_path, dst_path):
-            fct.copy_file_text(src_path, dst_path)
-            print(f'{dst_name} = {src_path} --> {dst_path}')
-        else:
-            print(f'{dst_name} \t= Remains')
+    if choose0 == 0:
+        break
     else:
-        print(f'{src_name} != {dst_name}')
+        # load json
+        try:
+            with open('filenotes.json', 'r') as fj:
+                filenotes = json.load(fj)
+                # print(filenotes)
+                src_root_dir = filenotes["src_root_dir"]
+                dst_root_dir = filenotes["dst_root_dir"]
+                src_list = filenotes["src_list"]
+                dst_list = filenotes["dst_list"]
+            # print(src_root_dir, src_list)
+            # print(dst_root_dir, dst_list)
+        except FileNotFoundError:
+            print('JSON BROKEN')
+            break
+        notes_n = len(dst_list)
+        if choose0 == 1:        # update
+            for n in range(notes_n):
+                src_name = fct.get_filename_from_path(src_list[n])
+                dst_name = fct.get_filename_from_path(dst_list[n])
+                if src_name == dst_name:
+                    src_path = Path(src_root_dir + src_list[n])
+                    dst_path = Path(dst_root_dir + dst_list[n])
+                    if not fct.file_cmp_text(src_path, dst_path):
+                        fct.copy_file_text(src_path, dst_path)
+                        print(f'{dst_name} = {src_path} ==> {dst_path}')
+                    else:
+                        print(f'{dst_name} \t= Remains')
+                else:
+                    print(f'{src_name} doesn\'t match {dst_name}')
+        elif choose0 == 2:      # show
+            print(f'Root dir: {src_root_dir} ==> {dst_root_dir}')
+            for n in range(notes_n):
+                print(f'{src_list[n]} ==> {dst_list[n]}')
+        elif choose0 == 4:      # change root directory
+            change_sp = Path(input(f'Source dir: {src_root_dir} --> '))
+            if change_sp.is_dir():
+                src_root_dir = change_sp
+            else:
+                print(f'Directory {change_sp} is not exist.')
+            change_dp = Path(input(f'Destination dir: {dst_root_dir} --> '))
+            if change_dp.is_dir():
+                dst_root_dir = change_dp
+            else:
+                print(f'Directory {change_dp} is not exist.')
 
 
-# src = 'test_class.py'
-# dst = 'test_class2.py'
-# different = not fct.file_cmp_text(src, dst)
-# if different:
-#     fct.copy_file_text(src, dst)
+    # refresh .json
+    filenotes["src_root_dir"] = src_root_dir
+    filenotes["dst_root_dir"] = dst_root_dir
+    filenotes["src_list"] = src_list
+    filenotes["dst_list"] = dst_list
+    with open('filenotes.json', 'w') as fj:
+        json.dump(filenotes, fj, indent=4, sort_keys=True)
 
 # end = input('\n==== END ====\nEnter anything to exit...')
